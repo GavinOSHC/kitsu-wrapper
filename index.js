6174196201e415eb232c24d257e15593dcdb3adf
gavin <gavinoshc@gmail.com>
2020-09-22 23:00:28 +0100

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const { anime } = require("./routes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(anime);
 
